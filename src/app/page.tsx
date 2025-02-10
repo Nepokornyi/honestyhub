3ecae991c21c32b969cc4e8b499626984ab0c1fc
Nepokornyi <vadymkin@gmail.com>
2025-02-10 12:23:51 +0100

    create footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
+import { Footer } from '@/components/Footer/Footer'
 import { Header } from '@/components/Header/Header'
 import { Portfolio } from '@/components/Portfolio/Portfolio'
-import { TextAnimation } from '@/components/TextAnimation/TextAnimation'
+import { BlobText } from '@/components/TextAnimation/BlobText'
 
 export default function Home() {
     return (
         <div className="relative flex flex-col justify-center w-full overflow-hidden">
             <Header />
-            <TextAnimation />
+            <BlobText />
             <Portfolio />
-            <div className="h-screen"></div>
-            <footer>Footer</footer>
+            <Footer />
         </div>
     )
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { TextAnimation } from '../TextAnimation/TextAnimation'
+
+export const Footer = () => {
+    return (
+        <>
+            <div className="marquee relative flex mx-6 lg:mx-12 py-8 text-4xl uppercase overflow-hidden">
+                <TextAnimation lineLength={5} renderArrow={false} />
+            </div>
+
+            <footer className="px-6 lg:px-12 py-6 flex justify-between">
+                <span className="item">©2025 HonestyHub</span>
+                <span>Design & Develop Studio</span>
+            </footer>
+        </>
+    )
+}
